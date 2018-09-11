18021a1d439cf528cbd285721733d1cdc94e7aca
어수혁 <32706557+SH-Broar@users.noreply.github.com>
2018-09-11 20:31:07 +0900

    Drill-03 end
    2017180021 어수혁
    https://youtu.be/ttUgbu94JoY
    
    "character_movesRect" is final code.

--- Drills/Drill - 03/character_movesRect.py
+++ Drills/Drill - 03/character_movesRect.py
@@ -5,7 +5,7 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-x = 20
+x = 400
 y = 90
 # fill here
 while(1):
@@ -13,25 +13,36 @@ while(1):
         clear_canvas_now()
         grass.draw_now(400,30)
         character.draw_now(x,y)
-        x = x + 2
-        delay(0.002)
+        x = x + 5
+        delay(0.02)
     while(y < 530):
         clear_canvas_now()
         grass.draw_now(400,30)
         character.draw_now(x,y)
-        y = y + 2
-        delay(0.002)
+        y = y + 5
+        delay(0.02)
     while(x > 20):
         clear_canvas_now()
         grass.draw_now(400,30)
         character.draw_now(x,y)
-        x = x - 2
-        delay(0.002)
+        x = x - 5
+        delay(0.02)
     while(y > 90):
         clear_canvas_now()
         grass.draw_now(400,30)
         character.draw_now(x,y)
-        y = y - 2
-        delay(0.002)
+        y = y - 5
+        delay(0.02)
+    while(x < 400):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,y)
+        x = x + 5
+        delay(0.02)
+    for t in range(0,62):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(400 + 200*math.sin(t/10),300 + -200*math.cos(t/10))
+        delay(0.05)
     
 close_canvas()
