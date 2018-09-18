fcb901ad30fbb53ec8eb31a4655f61857a6bf402
어수혁 <32706557+SH-Broar@users.noreply.github.com>
2018-09-18 18:25:48 +0900

    Move like Circle
    DO : Circular Moving, and It's almost Done of Drill - 03

--- Drills/Drill - 05/Drill05.py
+++ Drills/Drill - 05/Drill05.py
@@ -1,4 +1,5 @@
 from pico2d import *
+import math
 
 open_canvas()
 
@@ -66,7 +67,17 @@ def make_rectangle():
 
 
 def make_circle():
-    pass
+    cx, cy, r = 800 // 2, 600 // 2, (600-180) // 2
+    degree = -90
+    while degree < 270:
+        radian = math.radians(degree)
+        x = cx + r * math.cos(radian)
+        y = cy + r * math.sin(radian)
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        degree += 1
+        delay(0.01)
 
 
 while True:
