062157f273e4caa545e907abcdd3799e4b3a12e7
어수혁 <32706557+SH-Broar@users.noreply.github.com>
2018-09-18 18:42:23 +0900

    Drill 05 Init
    First Commit From drill 05

--- Drills/Drill - 05/Drill05.py
+++ Drills/Drill - 05/Drill05.py
@@ -7,5 +7,18 @@ grass = load_image('grass.png')
 character = load_image('character.png')
 
 
+def character_movetopoint(destX, destY):
+
+    pass
+
+
+DestinationList = [203, 535, 132, 243, 535, 470, 477, 203, 715, 136, 316, 225, 510, 92, 692, 518, 682, 336, 712, 349]
+
+
+while True:
+    index = 0
+    character_movetopoint(DestinationList[index],DestinationList[index+1])
+    index = index + 2
+
 
 close_canvas()
