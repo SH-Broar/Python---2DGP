134e18c1f3ff107e85d80f8e63c09f5cb40ac773
어수혁 <32706557+SH-Broar@users.noreply.github.com>
2018-09-18 18:51:06 +0900

    Make Movement of Basic
    DO : While true

--- Drills/Drill - 05/Drill05.py
+++ Drills/Drill - 05/Drill05.py
@@ -6,10 +6,32 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
+x = 100
+y = 50
+# init of global var
+
 
 def character_movetopoint(destX, destY):
-
+    global x
+    global y
+    while x != destX or y != destY:
+        if x < destX:
+            x += 1
+        if y < destY:
+            y += 1
+        if x > destX:
+            x -= 1
+        if y > destY:
+            y -= 1
+        character_rend()
     pass
+
+
+def character_rend():
+    clear_canvas_now()
+    grass.draw_now(400, 30)
+    character.draw_now(x, y)
+    delay(0.01)
 
 
 DestinationList = [203, 535, 132, 243, 535, 470, 477, 203, 715, 136, 316, 225, 510, 92, 692, 518, 682, 336, 712, 349]
@@ -19,6 +41,7 @@ while True:
     index = 0
     character_movetopoint(DestinationList[index],DestinationList[index+1])
     index = index + 2
+
 
 
 close_canvas()
