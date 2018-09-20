c57c99d5e5099b287bf370f8eb0956518d5d72ef
어수혁 <32706557+SH-Broar@users.noreply.github.com>
2018-09-20 18:41:06 +0900

    Drill 06 end - Make strict PEP 8 rule and refactoring
    2017180021 어수혁 Drill-06 end commit : 6 times
    https://youtu.be/UCl4R3Itv7o

--- Drills/Drill - 06/Drill 06.py
+++ Drills/Drill - 06/Drill 06.py
@@ -2,10 +2,16 @@ from pico2d import *
 
 KPU_WIDTH, KPU_HEIGHT = 600, 600
 
+running = True
+mx, my = KPU_WIDTH // 2, KPU_HEIGHT // 2
+x, y = 10, 10
+Mouse_clicked_x, Mouse_clicked_y = 10, 10
+frame = 0
+
 
 def handle_events():
     global running
-    global mx, my, clickx, clicky
+    global mx, my, Mouse_clicked_x, Mouse_clicked_y
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -13,17 +19,17 @@ def handle_events():
         elif event.type == SDL_MOUSEMOTION:
             mx, my = event.x, KPU_HEIGHT - 1 - event.y
         elif event.type == SDL_MOUSEBUTTONDOWN:
-            clickx, clicky = mx - 25, my + 26
+            Mouse_clicked_x, Mouse_clicked_y = mx - 25, my + 26
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
 
 def character_move():
-    global clickx, clicky, x, y
-    if clickx != x:
-        if clicky != y:
-            x += (clickx - x) / 10
-            y += (clicky - y) / 10
+    global Mouse_clicked_x, Mouse_clicked_y, x, y
+    if Mouse_clicked_x != x:
+        if Mouse_clicked_y != y:
+            x += (Mouse_clicked_x - x) / 10
+            y += (Mouse_clicked_y - y) / 10
 
 
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
@@ -31,15 +37,10 @@ kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
 mouse = load_image('hand_arrow.png')
 
-running = True
-mx, my = KPU_WIDTH // 2, KPU_HEIGHT // 2
-x, y = 10, 10
-clickx, clicky = 10, 10
-frame = 0
-hide_cursor()
 
 while running:
     clear_canvas()
+    hide_cursor()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
     mouse.draw(mx, my)
     character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
