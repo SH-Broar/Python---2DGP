2aecc4a705c851e8745c834e4caf76ba5e3204cb
어수혁 <32706557+SH-Broar@users.noreply.github.com>
2018-09-20 18:24:34 +0900

    Revert "Revert "Make Mouse Cursor""
    This reverts commit d2fe58eb2c7bd29a83e7cc5e66e7e1ee4f079029.

--- Drills/Drill 06.py
+++ Drills/Drill 06.py
@@ -1,39 +1,54 @@
 from pico2d import *
 
-KPU_WIDTH, KPU_HEIGHT = 1280, 1024
+KPU_WIDTH, KPU_HEIGHT = 600, 600
 
 
 def handle_events():
     global running
-    global x, y
+    global mx, my, clickx, clicky
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_MOUSEMOTION:
-            x, y = event.x, KPU_HEIGHT - 1 - event.y
+            mx, my = event.x, KPU_HEIGHT - 1 - event.y
+        elif event.type == SDL_MOUSEBUTTONDOWN:
+            clickx, clicky = mx - 25, my + 26
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
 
-open_canvas(KPU_WIDTH,KPU_HEIGHT)
+def character_move():
+    global clickx, clicky, x, y
+    if clickx != x:
+        if clicky != y:
+            x += (clickx-x)/10
+            y += (clicky-y)/10
+
+
+open_canvas(KPU_WIDTH, KPU_HEIGHT)
 kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
+mouse = load_image('hand_arrow.png')
 
 running = True
-x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
+mx, my = KPU_WIDTH // 2, KPU_HEIGHT // 2
+x, y = 10, 10
+clickx, clicky = 10, 10
 frame = 0
 hide_cursor()
 
 while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+    mouse.draw(mx, my)
     character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
     update_canvas()
     frame = (frame + 1) % 8
 
     delay(0.02)
     handle_events()
+    character_move()
 
 close_canvas()
 
