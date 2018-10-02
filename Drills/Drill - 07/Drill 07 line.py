e42142f9be72be862ad333724da4d9a15923e639
어수혁 <32706557+SH-Broar@users.noreply.github.com>
2018-10-02 18:50:22 +0900

    Make function draw_point
    it is character drawing func.

--- Drills/Drill - 07/Drill 07 line.py
+++ Drills/Drill - 07/Drill 07 line.py
@@ -12,11 +12,22 @@ flip = 0
 
 
 def draw_point(p):
-    pass
+    global frame
+    global flip
+    clear_canvas()
+    kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+    frame = (frame + 3) % 8
+
+    character.clip_draw(frame * 100, 100 * 1, 100, 100, p[0], p[1])
+
+    update_canvas()
+    delay(0.02)
 
 
 def draw_line_2_points(p1, p2):
     global flip
+
+    draw_point(p2)
     pass
 
 
@@ -30,12 +41,6 @@ points = [(random.randint(-500, 500), random.randint(-350, 350)) for i in range(
 while running:
     clear_canvas()
 
-    kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    frame = (frame + 3) % 8
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, 300, 300)
-
-    update_canvas()
-    delay(0.02)
     draw_line_2_points(points[frame-1], points[frame])
     frame = (frame+1) % 10
 
