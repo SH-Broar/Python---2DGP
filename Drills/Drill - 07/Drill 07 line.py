7093f274dfd14433e031be0c054bd9cb40bfc362
어수혁 <32706557+SH-Broar@users.noreply.github.com>
2018-10-02 18:46:27 +0900

    Make Struct of def
    Basis by Drill - 06

---
+++ Drills/Drill - 07/Drill 07 line.py
@@ -0,0 +1,39 @@
+from pico2d import *
+import random
+
+KPU_WIDTH, KPU_HEIGHT = 600, 600
+
+running = True
+mx, my = KPU_WIDTH // 2, KPU_HEIGHT // 2
+frame = 0
+pFrame = 0
+
+flip = 0
+
+
+def draw_point(p):
+    pass
+
+
+def draw_line_2_points(p1, p2):
+    global flip
+    pass
+
+
+open_canvas(KPU_WIDTH, KPU_HEIGHT)
+kpu_ground = load_image('KPU_GROUND.png')
+character = load_image('animation_sheet.png')
+
+size = 10
+points = [(random.randint(-500, 500), random.randint(-350, 350)) for i in range(size)]
+
+while running:
+    clear_canvas()
+    draw_line_2_points(points[frame-1], points[frame])
+    frame = (frame+1) % 10
+
+close_canvas()
+
+
+
+
