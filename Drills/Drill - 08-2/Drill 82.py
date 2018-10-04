fd0f41afdc21d17f02b5605f51581b7ebf79c6e6
어수혁 <32706557+SH-Broar@users.noreply.github.com>
2018-10-04 18:45:40 +0900

    almost stamped

--- Drills/Drill - 08-2/Drill 82.py
+++ Drills/Drill - 08-2/Drill 82.py
@@ -11,15 +11,28 @@ pFrame = 0
 pre = [0,0]
 flip = 0
 
+stamp = -1
+
+def draw_stamp():
+    global stamp
+    stamp = stamp + 8 % 8
+    for i in range(-3, stamp-3):
+        character.clip_draw(100, 100 * 1, 100, 100, points[i][0], points[i][1])
+    pass
 
 def draw_point(p):
     global frame
     global flip
+    global points
+    global stamp
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
     frame = (frame + 3) % 8
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, p[0], p[1])
-
+    if (flip == 0):
+        character.clip_draw(frame * 100, 100 * 1, 100, 100, p[0], p[1])
+    else:
+        character.clip_draw(frame * 100, 100 * 0, 100, 100, p[0], p[1])
+    draw_stamp()
     update_canvas()
     delay(0.02)
 
@@ -27,9 +40,11 @@ def draw_point(p):
 def draw_curve_5_points(p1, p2, p3, p4, p5):
     global pre
     global flip
+    global stamp
     x1, y1 = p1[0], p1[1]
     x2, y2 = p2[0], p2[1]
 
+    stamp = stamp + 1
     if (x1 > x2):
         flip = 1
     else:
@@ -43,6 +58,8 @@ def draw_curve_5_points(p1, p2, p3, p4, p5):
                     -3 * t ** 3 + 4 * t ** 2 + t) * p2[1] + (t ** 3 - t ** 2) * p3[1]) / 2
         draw_point((x, y))
     draw_point(p2)
+
+    stamp = stamp + 1
 
     x1, y1 = p2[0], p2[1]
     x2, y2 = p3[0], p3[1]
@@ -59,6 +76,7 @@ def draw_curve_5_points(p1, p2, p3, p4, p5):
         draw_point((x, y))
     draw_point(p3)
 
+    stamp = stamp + 1
     x1, y1 = p3[0], p3[1]
     x2, y2 = p4[0], p4[1]
 
