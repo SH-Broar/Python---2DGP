86aa067b206a013f29aae0eea37e15471b53bdfe
어수혁 <32706557+SH-Broar@users.noreply.github.com>
2018-10-04 18:28:56 +0900

    Restructed 8-2

--- Drills/Drill - 08-2/Drill 82.py
+++ Drills/Drill - 08-2/Drill 82.py
@@ -18,65 +18,13 @@ def draw_point(p):
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
     frame = (frame + 3) % 8
-    if (flip == 0):
-        character.clip_draw(frame * 100, 100 * 1, 100, 100, p[0], p[1])
-    else:
-        character.clip_draw(frame * 100, 100 * 0, 100, 100, p[0], p[1])
+
     update_canvas()
     delay(0.02)
 
 
 def draw_curve_5_points(p1, p2, p3, p4, p5):
-    global pre
-    global flip
-    x1, y1 = p1[0], p1[1]
-    x2, y2 = p2[0], p2[1]
-
-    if (x1 > x2):
-        flip = 1
-    else:
-        flip = 0
-    # draw p1-p2
-    for i in range(0, 100, 2):
-        t = i / 100
-        x = ((-t ** 3 + 2 * t ** 2 - t) * p5[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p1[0] + (
-                    -3 * t ** 3 + 4 * t ** 2 + t) * p2[0] + (t ** 3 - t ** 2) * p3[0]) / 2
-        y = ((-t ** 3 + 2 * t ** 2 - t) * p5[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p1[1] + (
-                    -3 * t ** 3 + 4 * t ** 2 + t) * p2[1] + (t ** 3 - t ** 2) * p3[1]) / 2
-        draw_point((x, y))
-    draw_point(p2)
-
-    x1, y1 = p2[0], p2[1]
-    x2, y2 = p3[0], p3[1]
-
-    if (x1 > x2):
-        flip = 1
-    else:
-        flip = 0
-    # draw p2-p3
-    for i in range(0, 100, 2):
-        t = i / 100
-        x = ((-t**3 + 2*t**2 - t)*p1[0] + (3*t**3 - 5*t**2 + 2)*p2[0] + (-3*t**3 + 4*t**2 + t)*p3[0] + (t**3 - t**2)*p4[0])/2
-        y = ((-t**3 + 2*t**2 - t)*p1[1] + (3*t**3 - 5*t**2 + 2)*p2[1] + (-3*t**3 + 4*t**2 + t)*p3[1] + (t**3 - t**2)*p4[1])/2
-        draw_point((x, y))
-    draw_point(p3)
-
-    x1, y1 = p3[0], p3[1]
-    x2, y2 = p4[0], p4[1]
-
-    if (x1 > x2):
-        flip = 1
-    else:
-        flip = 0
-    # draw p3-p4
-    for i in range(0, 100, 2):
-        t = i / 100
-        x = ((-t ** 3 + 2 * t ** 2 - t) * p2[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p3[0] + (
-                    -3 * t ** 3 + 4 * t ** 2 + t) * p4[0] + (t ** 3 - t ** 2) * p5[0]) / 2
-        y = ((-t ** 3 + 2 * t ** 2 - t) * p2[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p3[1] + (
-                    -3 * t ** 3 + 4 * t ** 2 + t) * p4[1] + (t ** 3 - t ** 2) * p5[1]) / 2
-        draw_point((x, y))
-    draw_point(p4)
+    pass
 
 
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
