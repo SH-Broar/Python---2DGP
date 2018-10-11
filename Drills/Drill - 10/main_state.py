a4a7e545f45f8ea70cd3ab11235da0ee19a00a42
어수혁 <32706557+SH-Broar@users.noreply.github.com>
2018-10-11 18:49:24 +0900

    pause_state
    well it's work now?

--- Drills/Drill - 10/main_state.py
+++ Drills/Drill - 10/main_state.py
@@ -6,7 +6,7 @@ from pico2d import *
 
 import game_framework
 import title_state
-#import pause_state
+import pause_state
 
 
 name = "MainState"
@@ -14,6 +14,7 @@ name = "MainState"
 boy = None
 grass = None
 font = None
+
 
 class Grass:
     def __init__(self):
@@ -71,8 +72,8 @@ def handle_events():
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.change_state(title_state)
-        #elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_p):
-            #game_framework.push_state(pause_state)
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_p):
+            game_framework.push_state(pause_state)
 
 
 def update():
