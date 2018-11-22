d05abfa564ad234c42a0488a1099d68fbc9627d7
어수혁 <32706557+SH-Broar@users.noreply.github.com>
2018-11-22 17:36:45 +0900

    Drill 14 init

--- Drills/Drill - 13/boy.py
+++ Drills/Drill - 13/boy.py
@@ -119,6 +119,9 @@ class Boy:
         self.cur_state = WalkingState
         self.cur_state.enter(self, None)
 
+        self.eat_sound = load_wav('pickup.wav')
+        self.eat_sound.set_volume(32)
+
     def get_bb(self):
         return self.x - 50, self.y - 50, self.x + 50, self.y + 50
 
