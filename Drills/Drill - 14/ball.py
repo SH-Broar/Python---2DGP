44b346e1a26f816e61962b8add44034e0a7ce085
어수혁 <32706557+SH-Broar@users.noreply.github.com>
2018-11-22 18:56:40 +0900

    Revert "Revert "side block""
    This reverts commit 2c369bdf63c6cf374d3f8cc9e766a13f79725f41.

--- Drills/Drill - 14/ball.py
+++ Drills/Drill - 14/ball.py
@@ -2,6 +2,7 @@ import random
 from pico2d import *
 import game_world
 import game_framework
+import main_state
 
 class Ball:
     image = None
@@ -9,15 +10,16 @@ class Ball:
     def __init__(self):
         if Ball.image == None:
             Ball.image = load_image('ball21x21.png')
-        self.x, self.y, self.fall_speed = random.randint(0, 1600-1), 60, 0
-
+        self.x, self.y, self.fall_speed = random.randint(400, 1400), random.randint(300, 800), 0
+        self.x += 400
+        self.y += 300
     def get_bb(self):
-        return self.x - 10, self.y - 10, self.x + 10, self.y + 10
+        return self.x- main_state.boy.x - 10, self.y- main_state.boy.y - 10, self.x-main_state.boy.x + 10, self.y- main_state.boy.y + 10
 
     def draw(self):
-        self.image.draw(self.x, self.y)
+        self.image.draw(self.x- main_state.boy.x, self.y- main_state.boy.y)
         draw_rectangle(*self.get_bb())
 
     def update(self):
-        self.y -= self.fall_speed * game_framework.frame_time
+        pass
 
